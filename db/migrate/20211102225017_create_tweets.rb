fb7ee703743280bc04702f9a20b24047774b7af9
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-11-02 18:57:35 -0400

    Commit 3: Successfully run the Tweets migration

--- db/migrate/20211102225017_create_tweets.rb
+++ db/migrate/20211102225017_create_tweets.rb
@@ -1,8 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
-
-
+	    t.string   "title"
+	    t.text     "content"
+	    t.string   "author"
     end
   end
 end
