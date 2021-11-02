c4519fe45d9fa4e5011e2be8ba8662996db16eb7
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-11-02 19:15:49 -0400

    Commit 8: Can POST new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,19 @@ class TweetsController < ApplicationController
         render(json: {tweet: tweet})
     end
 
+    def create
+        tweet = Tweet.new(tweet_params)
+
+        if tweet.save
+            render json: {tweet: tweet}
+        else
+            render(status: 422, json: {tweet: tweet, errors: tweet.errors})
+        end
+    end
+    
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:title, :content, :author)
 end
+
